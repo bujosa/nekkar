06019aa69025b1581f380c0998430cd2e332e5fc
bujosa <davidbujosa@gmail.com>
2023-05-19 01:55:47 -0400

    feat: add first example using surrealdb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+use anyhow::{anyhow, Result};
+use std::collections::BTreeMap;
+use surrealdb::sql::{thing, Datetime, Object, Thing, Value};
+use surrealdb::{Datastore, Response, Session};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let ds = Datastore::new("memory").await?;
+
+    let session = Session::for_db("test", "test");
+
+    let sql = "CREATE task:1 SET title = 'test', done = false";
+
+    let response = ds.execute(sql, &session, None, false).await?;
+
+    println!("{:?}", response);
+
+    Ok(())
 }
